84feabc697f484845b5b7ecb293bbec1bfd4a3d8
Kiliwwwww <1154798056@qq.com>
2020-04-13 16:53:24 +0800

    set file

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -1,7 +1,12 @@
 package test;
 
+import java.io.*;
+
 public class Test {
-    public static void main(String[] args) {
-        System.out.println("hello git!");
+    public static void main(String[] args) throws IOException {
+        File file = new File(Test.class.getClassLoader().getResource("1.txt").getPath());
+        BufferedReader bis = new BufferedReader(new FileReader(file));
+        String string = bis.readLine();
+        System.out.println(string);
     }
 }
